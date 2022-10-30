777bf91b03b1c403797cfca41b4d4526a9916dc8
Ziad Bahy <ziad.amer06@gmail.com>
2022-10-30 20:30:55 +0200

    Finished the 3 admin tasks following the MVC model

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
+require("dotenv").config({path: "./config.env"});
+const port = process.env.PORT;
+
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 const cors = require("cors");
-require("dotenv").config({path: "./config.env"});
-const port = process.env.PORT || 5000;
+
+const adminRoutes = require('./routes/admin');
+
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+//app.use(require("./routes/record"));
+app.use('/', adminRoutes);
+
 //get driver connection
 const dbo = require("./db/conn");
+
 
 app.listen(port, () => {
     //perform a databse connection when the server starts
