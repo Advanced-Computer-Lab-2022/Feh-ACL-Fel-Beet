1daea8aa8dc98a662f1cddeee7cc219f0fed9808
Yousef Korayem <yousefkorayem1@gmail.com>
2022-10-31 11:59:57 +0200

    Fixed bad merging of Zizo's branch and Omar Yehia's branch

--- server/server.js
+++ server/server.js
@@ -1,20 +1,8 @@
 require("dotenv").config({path: "./config.env"});
 
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
-
-const adminRoutes = require('./routes/admin');
-
-app.use(cors());
-app.use(express.json());
-//app.use(require("./routes/record"));
-app.use('/', adminRoutes);
-
-const express = require("express");
-const Routes = require('./routes/record')
 const port = process.env.PORT || 5000;
-const cors = require("cors")
+const cors = require("cors");
 
 //get driver connection
 const dbo = require("./db/conn");
@@ -30,12 +18,12 @@ app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
-  })
+  }
+)
   
-
-
 //routes
 app.use(require("./routes/record"));
+app.use(require('./routes/admin'));
 
 
 // connect to db
