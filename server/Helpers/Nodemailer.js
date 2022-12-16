e521080db5288e465fd366ee7c80f849c22f4b6b
Ziad Bahy <ziad.amer06@gmail.com>
2022-12-16 14:29:16 +0200

    Automated mails are now added

---
+++ server/Helpers/Nodemailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: 'feh-ACL-fel-beet@outlook.com',
+        pass: 'OmarlikesHomer'
+    }
+});
+
+const sendMail = async (emailAddress, subject, message) => {
+    
+    try {
+        let info = await transporter.sendMail({
+            from: 'feh-ACL-fel-beet@outlook.com', // sender address
+            to: emailAddress, // list of receivers
+            subject: subject, // Subject line
+            text: message, // plain text body
+        });
+
+        console.log(info)
+        return true;
+        
+    } catch (e) {
+       console.log(e);
+        return e.message;
+        
+    }
+}
+
+sendMail('ziad.amer06@gmail.com', "Hope this works", "It works!");
+
+module.exports = sendMail;
