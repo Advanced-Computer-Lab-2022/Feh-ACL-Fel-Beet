41afcbc970c4e3b10d24c6684169a5823416202f
Omar Yehia <106839329+omarkab2@users.noreply.github.com>
2022-12-23 15:12:12 +0200

    Merge Commit

--- server/Routes/individualTraineeRoutes.js
+++ server/Routes/individualTraineeRoutes.js
@@ -1,17 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const { add, login, viewRatingsAndReviews, writeReview, findIndividualTrainee } = require('../Controllers/individualTraineeController')
-const { rateInstructor, rateCourse } = require('../Controllers/traineeController')
+const express = require("express");
+const router = express.Router();
+const {
+  rateInstructor,
+  rateCourse,
+} = require("../Controllers/traineeController");
 
-router.post('/add', add)
-router.post('/login', login)
-router.post('/findIndividualTrainee', findIndividualTrainee)
-router.post('/viewRatingsAndReviews/:id', viewRatingsAndReviews)
-router.post('/rateInstructor/:traineeId/:instructorId', rateInstructor)
+router.post("/rateInstructor/:traineeId/:instructorId", rateInstructor);
 
 // Rate a course
-router.post('/rateCourse/:traineeId/:courseId', rateCourse);
+router.post("/rateCourse/:traineeId/:courseId", rateCourse);
 
-
-
-module.exports = router
+module.exports = router;
