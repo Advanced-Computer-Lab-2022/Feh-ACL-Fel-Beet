6d0c2de2b8b7185d2b533bba41fb00bbb5d349da
Omar Yehia <106839329+omarkab2@users.noreply.github.com>
2022-12-23 21:43:03 +0200

    Improved Styling For Home Page

--- frontend/src/components/CourseDetails.js
+++ frontend/src/components/CourseDetails.js
@@ -1,15 +1,29 @@
-const CourseDetails = ({ course }) => {
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-    return (
-      <div className="course-details">
-        <h4>{course.Name}</h4>
-        <p><strong>Total Hours: </strong>{course.Hours}</p>
-        <p><strong>Total Rating: </strong>{course.totalRating}</p>
-        <p><strong>Number of Ratings: </strong>{course.noOfRatings}</p>
-        <p><strong>Price: </strong>{course.Price}</p>
-        <p>{course.createdAt}</p>
-      </div>
-    )
-  }
-  
-  export default CourseDetails
+const CourseDetails = ({ course }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="course-details" style={{cursor:'pointer', marginLeft:'4%'}} onClick={() => navigate('../login')}>
+      <h4>{course.Name}</h4>
+      <p>
+        <strong>Total Hours: </strong>
+        {course.Hours}
+      </p>
+      <p>
+        <strong>Total Rating: </strong>
+        {course.totalRating}
+      </p>
+      <p>
+        <strong>Number of Ratings: </strong>
+        {course.noOfRatings}
+      </p>
+      <p>
+        <strong>Price: </strong>
+        {course.Price}
+      </p>
+    </div>
+  );
+};
+
+export default CourseDetails;
